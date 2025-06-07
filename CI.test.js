1042a2cd2d536d662f7d7c9ede70244fa9870160
Locochom <126177808+Locochom@users.noreply.github.com>
2025-06-06 19:47:40 -0500

    Correcciones

--- CI.test.js
+++ CI.test.js
@@ -1,11 +1,11 @@
-const cadenaInversa = require('./cadenaInversa');
+const cadenaInversa = require('./CI');
 test('invierte la cadena  "hola" a "aloh"',() => {
     expect(cadenaInversa('hola')).toBe('aloh');
 });
 
-test('invierte la cadena vacía a cadena vacía'),()=>{
+test('invierte la cadena vacía a cadena vacía', () => {
     expect(cadenaInversa('')).toBe('');
-};
+});
 
 test('invierte la cadena con espacios "abc def" a "fed cba"',() =>{
     expect(cadenaInversa('abc def')).toBe('fed cba');
